5dedd5bd5adb16ab8ca7cd09c3d721f67aaca5a2
Daniyal Issin <daniyal.isin79@gmail.com>
2022-01-13 21:32:42 -0700

    Added recipe model

--- app/core/admin.py
+++ app/core/admin.py
@@ -21,6 +21,7 @@ class UserAdmin(BaseUserAdmin):
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Tag)
 admin.site.register(models.Ingredient)
+admin.site.register(models.Recipe)
 
 
 
